7346e02a6d62785d2c3a08537fed249471f3e8cf
Diego Aldair García Hernández <147885940+DiegoAldair01@users.noreply.github.com>
2025-04-12 01:12:02 -0600

    Adicion de archivos de práctica 8
    Se agregan archivos correspondientes a la práctica 8.

---
+++ Window.h
@@ -0,0 +1,54 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);
+	}
+	bool* getsKeys() { return keys; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	GLfloat getrotay() { return rotay; }
+	GLfloat getrotax() { return rotax; }
+	GLfloat getrotaz() { return rotaz; }
+	GLfloat getarticulacion1() { return articulacion1; }
+	GLfloat getarticulacion2() { return articulacion2; }
+	GLfloat getmoverse() { return moverse; }
+	GLfloat getmoverseH() { return moverseH; }
+	GLfloat getvalor() { return valor; }
+	GLfloat getfaro() { return faro_enc; }
+	GLfloat getballon() { return balloon_enc; }
+
+
+
+
+	~Window();
+private:
+	GLFWwindow* mainWindow;
+	GLint width, height;
+	GLfloat rotax, rotay, rotaz, articulacion1, articulacion2, moverse, moverseH;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat valor;
+	GLfloat balloon_enc;
+	GLfloat faro_enc;
+	bool mouseFirstMoved;
+	void createCallbacks();
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+};
